857b905ce2f4112f6320de2febdd1634624589e6
jakzaizzat <jakzaizzat@gmail.com>
2022-04-05 16:59:58 +0800

    fix stroke width naming

--- components/Receipt.tsx
+++ components/Receipt.tsx
@@ -35,7 +35,7 @@ const Receipt = ({ address }: Props) => {
             cy="12"
             r="10"
             stroke="currentColor"
-            stroke-width="4"
+            strokeWidth="4"
           ></circle>
           <path
             className="opacity-75"
